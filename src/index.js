eb49a2d7074ea8084de249d18617a89663c8d286
Rahul Bhatia <112072457+rahulbhatia3422@users.noreply.github.com>
2025-07-26 10:41:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Node.js app! Hello I am Rahul...');
+  res.send('Hello from DevOps Node.js app! Hello I am Rahul... , Welcome to DevOps');
 });
 
 app.listen(port, () => {
