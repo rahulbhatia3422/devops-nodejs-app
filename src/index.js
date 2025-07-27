926cc0202bb1e3629d5fb9c08b9daf838416320c
Rahul Bhatia <112072457+rahulbhatia3422@users.noreply.github.com>
2025-07-27 20:18:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     <html lang="en">
     <head>
       <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
       <title>DevOps Node.js App</title>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
